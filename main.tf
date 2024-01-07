6cc2296e03c3272e56469ee7d7c672276eda6e30
itkarank <146817930+itkarank@users.noreply.github.com>
2024-01-07 12:14:55 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "my_instance" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = "subnet-0ba60291691470713"
-  key_name      = var.key_name
+  
   security_groups =  [aws_security_group.instance_sg.id]
   associate_public_ip_address = true
 
