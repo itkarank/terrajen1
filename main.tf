c208699ff777ce5f51c1f4ac3b3f29aa1eb9f052
itkarank <146817930+itkarank@users.noreply.github.com>
2024-01-07 11:09:00 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+# Amazon Linux EC2 Instance
+
+resource "aws_instance" "my_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.public_subnet.id
+  key_name      = var.key_name
+  security_group = [aws_security_group.instance_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
+
+
+
+
+
