51459086dd0f7989a257983752127babd7b7b0a4
itkarank <146817930+itkarank@users.noreply.github.com>
2024-01-07 11:55:13 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@
 resource "aws_instance" "my_instance" {
   ami           = var.ami
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.public_subnet.id
+  subnet_id     = "subnet-0ba60291691470713"
   key_name      = var.key_name
-  security_groups = [aws_security_group.instance_sg.id]
+  security_groups = ["sg-0ac504d26529d9e79"]
   associate_public_ip_address = true
 
   tags = {
