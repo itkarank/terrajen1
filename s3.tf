52902e59c181debf9401cd34d24062d8f347e1d4
itkarank <146817930+itkarank@users.noreply.github.com>
2024-01-07 20:41:51 -0800

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -5,3 +5,11 @@ resource "aws_s3_bucket" "b" {
     Name        = "My tf bucket"
   }
 }
+
+ backend "s3" {
+bucket = "my-tf-testkris-bucket"
+   key = "terraform.tfstate"
+   region = "us-east-2"
+   }
+}
+   
