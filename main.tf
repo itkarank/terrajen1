7917843df33067b8f881b12fba1c6cf48608d280
itkarank <146817930+itkarank@users.noreply.github.com>
2024-02-24 08:12:10 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
 # Amazon Linux EC2 Instance
 
 resource "aws_instance" "my_instance" {
+count = 2
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = "subnet-0ba60291691470713"
-  
+ key_name      = "newkey"
   security_groups =  [aws_security_group.instance_sg.id]
   associate_public_ip_address = true
 
   tags = {
-    Name = "MyEC2Instance"
+    Role  =  count.index == 0 ? "CI/CD server" : "production server"
   }
 }
 
